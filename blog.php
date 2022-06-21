e63c965a4dfd2d78aa8c44692df694f7b2913993
Armando Martinez <armando@tecuani.me>
2022-06-21 18:26:34 -0500

    finishing touches

---
+++ blog.php
@@ -0,0 +1,25 @@
+<?php
+    $name = $_POST['nombre'];
+    $email = $_POST['correo'];
+    $web = $_POST['web'];
+    $message = $_POST['mensaje'];
+
+    $subject = "Comentario Blog";
+
+    $header = 'From: ' . $email . " \r\n";
+    $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+    $header .= "Mime-Version: 1.0 \r\n";
+    $header .= "Content-Type: text/plain";
+
+    $message = "Este mensaje fue enviado por: " . $name . " \r\n";
+    $message .= "Su e-mail es: " . $email . " \r\n";
+    $message .= "Mensaje: " . $_POST['message'] . " \r\n";
+    $message .= "Enviado el: " . date('d/m/Y', time());
+
+    $para = 'armando@tecuani.me';
+
+    mail($para, $subject, utf8_decode($message), $header);
+    echo '<script>alert("tu mensaje ha sido enviado");
+        window.location.href = "index.html";
+    </script>';
+?>
---
+++ send.php
@@ -0,0 +1,36 @@
+<?php
+    $name = $_POST['nombre'];
+    $apellidos = $_POST['appellidos'];
+    $phone = $_POST['numero'];
+    $email = $_POST['correo'];
+    $pais = $_POST['pais'];
+    $adultos = $_POST['adultos'];
+    $ninos = $_POST['ninos'];
+    $destino = $_POST['destino'];
+    $llegada = $_POST['llegada'];
+    $hora = $_POST['hora'];
+    $message = $_POST['mensaje'];
+    
+    $subject = "Cotizacion";
+
+    $header = 'From: ' . $email . " \r\n";
+    $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+    $header .= "Mime-Version: 1.0 \r\n";
+    $header .= "Content-Type: text/plain";
+
+    $message = "Este mensaje fue enviado por: " . $name . $apellidos .  " \r\n";
+    $message .= "Su e-mail es: " . $email . " \r\n";
+    $message .= "Su telefono es: " . $phone . " \r\n";
+    $message .= "Pais de origen: " . $pais . " \r\n";
+    $message .= "No. de adultos: " . $adultos . " \r\n";
+    $message .= "No. de niños: " . $ninos . " \r\n";
+    $message .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+    $message .= "Enviado el: " . date('d/m/Y', time());
+
+    $para = 'armando@tecuani.me';
+
+    mail($para, $subject, utf8_decode($message), $header);
+    echo '<script>alert("Tu mensaje ha sido enviado");
+        window.location.href = "index.html";
+    </script>';
+?>
